960f3ae8383bfb3d5ee33911655e8cf9ff099812
bearthatcares <149488155+bearthatcares@users.noreply.github.com>
2023-12-03 18:08:36 -0500

    Update discord_uid_lookup.sql

--- discord_uid_lookup.sql
+++ discord_uid_lookup.sql
@@ -4,9 +4,17 @@ WITH user_groups as (SELECT aug.user_id, ag.name
                               join auth_group ag on ag.id = aug.group_id),
      characters AS (SELECT ac.user_id, ee.character_id, ee.character_name
                     from eveonline_evecharacter ee
-                             join authentication_characterownership ac on ac.character_id = ee.id)
-SELECT p.uid as discord_uid, GROUP_CONCAT(c.character_id) AS character_ids, GROUP_CONCAT(s.name) AS roles
+                             join authentication_characterownership ac on ac.character_id = ee.id),
+     main_characters AS (SELECT aup.user_id, e.character_id
+                         FROM authentication_userprofile aup
+                                  INNER JOIN alliance_auth.eveonline_evecharacter e on aup.main_character_id = e.id
+                         WHERE main_character_id is not null)
+SELECT p.uid                        as discord_uid,
+       mc.character_id              as main_character_id,
+       GROUP_CONCAT(c.character_id) AS character_ids,
+       GROUP_CONCAT(s.name)         AS roles
 FROM user_groups s
          INNER JOIN discord_discorduser p ON (s.user_id = p.user_id)
          INNER JOIN characters c ON (s.user_id = c.user_id)
+         INNER JOIN main_characters mc ON (s.user_id = mc.user_id)
 GROUP BY p.uid;
